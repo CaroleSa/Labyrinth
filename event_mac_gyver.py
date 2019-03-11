4a1e294ec7081aae6d4d59645a366d4da7674208
CaroleSa <carole220287@hotmail.com>
2019-03-11 21:39:54 +0100

    event_mac_gyver.py creation

---
+++ event_mac_gyver.py
@@ -0,0 +1,26 @@
+import os
+from labyrinth_position import *
+
+
+
+
+
+
+
+
+
+    """finale_position_dict = ("{}: {}".format(cle+4, valeur+20))
+    """
+    print(mouv_dict)
+
+
+mouv_dict={0: 520}
+while :
+    if event.key == K_RIGHT:
+        mac_gyver_location = mac_gyver_location.move(40, 0)
+        for cle, valeur in mouv_dict.items():
+            mouv_dict[cle+40]=valeur
+            finale_position= ("({}, {})".format(cle+4, valeur+20))
+            init_event(finale_position) # rafraichissement et recollage des images
+
+

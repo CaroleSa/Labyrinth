658309defcea7a68bf539cc51e6ac30aed902052
CaroleSa <carole220287@hotmail.com>
2019-03-11 21:35:44 +0100

    new random_position.py for test git push : because the file does not register

---
+++ random_position.py
@@ -0,0 +1,45 @@
+import os
+from random import *
+import pygame
+from pygame.locals import *
+from labyrinth_position import *
+
+pygame.init() 
+
+
+class random_position:
+    def __init__(self):
+        line_liste = [line_1, line_2, line_3, line_4, line_5, line_6, line_7, line_8,
+             line_9, line_10, line_11, line_12, line_13
+]
+        random_line = choice(line_liste)
+        line_finale=[]
+        i=0             # i : notre compteur
+        for i, elt in enumerate(random_line):     # va permettre de parcourir tous les élèments de la ligne définie dans les paramètres 
+            if elt == "6": 
+                index_random_line = line_liste.index(random_line)                  # si un des élèments correspond au number_picture demandé,
+                line_finale.append(((i*40), (index_random_line+1)*40))     # les coordonnées de son emplacement s'ajoute à ma liste vide
+        self.random_location = choice(line_finale)
+        i+=1  
+       
+needle_location=random_position()
+ether_location=random_position()
+syringe_location=random_position()
+needle_position = needle_location.random_location
+ether_position = ether_location.random_location
+syringe_position = syringe_location.random_location
+
+while needle_position == ether_position or ether_position == syringe_position or syringe_position == needle_position:
+    needle_location=random_position()
+    ether_location=random_position()
+    syringe_location=random_position()
+    needle_position = needle_location.random_location
+    ether_position = ether_location.random_location
+    syringe_position = syringe_location.random_location
+    while needle_position == (0,520) or ether_position == (0,520) or syringe_position == (0,520):
+        needle_location=random_position()
+        ether_location=random_position()
+        syringe_location=random_position()
+        needle_position = needle_location.random_location
+        ether_position = ether_location.random_location
+        syringe_position = syringe_location.random_location

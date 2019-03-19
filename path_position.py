1b61a2b8521e85e27fb96000e0829853c15a2404
CaroleSa <carole220287@hotmail.com>
2019-03-19 16:33:52 +0100

    message path_position test new branch

--- path_position.py
+++ path_position.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 """creation of a list of coordinates of the labyrinth path"""
-
+NOUVELLE BRANCHE
 from labyrinth_position import *  # import lists that visually represent the game background
 
 # empty list that will indicate the coordinates of the labyrinth path

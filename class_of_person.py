778d892020b62d70ca175e875796b6c0dce191fc
CaroleSa <carole220287@hotmail.com>
2019-03-28 10:55:08 +0100

    labyrinth_game.py pylint ok

--- class_of_person.py
+++ class_of_person.py
@@ -22,9 +22,9 @@ class Person():
         """download pictures"""
         # instantiate the class Labyrinth
         self.labyrinth = class_of_labyrinth.Labyrinth()
-        self.labyrinth_entry = self.labyrinth.labyrinth_entry 
+        self.labyrinth_entry = self.labyrinth.labyrinth_entry
         self.labyrinth_exit = self.labyrinth.labyrinth_exit
-        self.window = self.labyrinth.window 
+        self.window = self.labyrinth.window
         # instantiate the class Objects
         self.objects = class_of_objects.Objects()
         self.position_object_1 = self.objects.position_object_1
@@ -33,7 +33,7 @@ class Person():
         self.object_1 = self.objects.object_1
         self.object_2 = self.objects.object_2
         self.object_3 = self.objects.object_3
-        # download pictures     
+        # download pictures
         self.grave_picture = pygame.image.load("grave.png").convert()
         self.replay_picture = pygame.image.load("replay.png").convert()
         self.won_picture = pygame.image.load("won.png").convert()
@@ -50,24 +50,24 @@ class Person():
         # = 1 when Mac Gyver finds the object
         self.pick_up_object_1 = 0
         self.pick_up_object_2 = 0
-        self.pick_up_object_3 = 0 
+        self.pick_up_object_3 = 0
         # creating a rect
         self.moving_mac_gyver = self.mac_gyver_picture.get_rect()
 
     def color_blit_person(self):
         """transparency of the background and blit of the pictures : guardian and Mac Gyver"""        
-        self.guardian_picture.set_colorkey((255, 255, 255))       
+        self.guardian_picture.set_colorkey((255, 255, 255))
         self.window.blit(self.guardian_picture, self.labyrinth_exit)
         self.mac_gyver_picture.set_colorkey((255, 255, 255))
         self.window.blit(self.mac_gyver_picture, self.last_location_mac_gyver_tuple)
 
     def color_pictures_end_game(self):
-        """transparency of the background : pictures of the end of the game""" 
+        """transparency of the background : pictures of the end of the game"""
         self.grave_picture.set_colorkey((255, 255, 255))
         self.won_picture.set_colorkey((255, 255, 255))
         self.quit_picture.set_colorkey((255, 255, 255))
         self.replay_picture.set_colorkey((255, 255, 255))
-                
+
     def init_event(self):
         """blit after the events"""
         self.labyrinth.blit_pictures()
@@ -77,7 +77,7 @@ class Person():
         pygame.display.flip()
 
     def movement_right(self):
-        """Mac Gyver turns to the right, update of the list path_traveled_mac_gyver""" 
+        """Mac Gyver turns to the right, update of the list path_traveled_mac_gyver"""
         # moving of Mac Gyver and blit all pictures
         for key, value in self.last_location_mac_gyver_dict.items():
             self.moving_mac_gyver = self.moving_mac_gyver.move(40, 0)
@@ -86,7 +86,7 @@ class Person():
             self.last_location_mac_gyver_dict[key + 40] = value
             self.path_traveled_mac_gyver.append(self.last_location_mac_gyver_tuple)
             self.init_event()
-        # Mac Gyver avoids the right wall 
+        # Mac Gyver avoids the right wall
         if self.labyrinth.path_location().count(self.last_location_mac_gyver_tuple) == 0:
             for key, value in self.last_location_mac_gyver_dict.items():
                 self.moving_mac_gyver = self.moving_mac_gyver.move(- 40, 0)
@@ -97,7 +97,7 @@ class Person():
                 self.init_event()
 
     def movement_left(self):
-        """Mac Gyver turns to the left, update of the list path_traveled_mac_gyver""" 
+        """Mac Gyver turns to the left, update of the list path_traveled_mac_gyver"""
         # moving of Mac Gyver and blit all pictures
         for key, value in self.last_location_mac_gyver_dict.items():
             self.moving_mac_gyver = self.moving_mac_gyver.move(- 40, 0)
@@ -115,9 +115,9 @@ class Person():
                 self.last_location_mac_gyver_dict.clear()
                 self.last_location_mac_gyver_dict[key + 40] = value
                 self.init_event()
-                    
+
     def movement_up(self):
-        """Mac Gyver goes upstairs, update of the list path_traveled_mac_gyver""" 
+        """Mac Gyver goes upstairs, update of the list path_traveled_mac_gyver"""
         # moving of Mac Gyver and blit all pictures
         for key, value in self.last_location_mac_gyver_dict.items():
             self.moving_mac_gyver = self.moving_mac_gyver.move(0, - 40)
@@ -137,7 +137,7 @@ class Person():
                 self.init_event()
 
     def movement_down(self):
-        """Mac Gyver goes downstairs, update of the list path_traveled_mac_gyver""" 
+        """Mac Gyver goes downstairs, update of the list path_traveled_mac_gyver"""
         # moving of Mac Gyver and blit all pictures
         for key, value in self.last_location_mac_gyver_dict.items():
             self.moving_mac_gyver = self.moving_mac_gyver.move(0, 40)
@@ -159,8 +159,8 @@ class Person():
     def keep_still(self):
         """if Mac Gyver arrives on the guardien, he can no longer move"""
         column = self.labyrinth.labyrinth_exit[0]
-        line = self.labyrinth.labyrinth_exit[1] 
-        # if Mac Gyver goes over the guardian   
+        line = self.labyrinth.labyrinth_exit[1]
+        # if Mac Gyver goes over the guardian
         if self.path_traveled_mac_gyver.count(self.labyrinth_exit) == 1 \
         and self.last_location_mac_gyver_dict == {column: line - 40}:
             for key, value in self.last_location_mac_gyver_dict.items():
@@ -168,7 +168,7 @@ class Person():
                 self.last_location_mac_gyver_dict.clear()
                 self.last_location_mac_gyver_dict[key] = value + 40
                 self.init_event()
-        # if Mac Gyver goes below the guardian 
+        # if Mac Gyver goes below the guardian
         if self.path_traveled_mac_gyver.count(self.labyrinth_exit) == 1 \
         and self.last_location_mac_gyver_dict == {column: line + 40}:
             for key, value in self.last_location_mac_gyver_dict.items():
@@ -219,7 +219,7 @@ class Person():
         lost = 1
         if self.last_location_mac_gyver_tuple == self.labyrinth_exit and self.counter_objects != 3:
             self.labyrinth.blit_pictures()
-            self.guardian_picture.set_colorkey((255, 255, 255))       
+            self.guardian_picture.set_colorkey((255, 255, 255))
             self.window.blit(self.guardian_picture, self.labyrinth_exit)
             self.color_pictures_end_game()
             self.window.blit(self.grave_picture, (200, 200))
@@ -227,12 +227,12 @@ class Person():
             self.window.blit(self.quit_picture, (520, 520))
             pygame.display.flip()
             return lost
-        
+
     def won(self):
         """if Mac Gyver wins, blit pictures"""
         won = 1
         if self.last_location_mac_gyver_tuple == self.labyrinth_exit and self.counter_objects == 3:
-            self.labyrinth.blit_pictures()  
+            self.labyrinth.blit_pictures()
             self.color_pictures_end_game()
             self.window.blit(self.won_picture, (120, 120))
             pygame.display.flip()
